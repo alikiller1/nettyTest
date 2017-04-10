2958d739adc2a1268555fd3d4054bc832bf94156
alikiller1 <liu2011321@sina.com>
2017-04-10 23:16:40 +0800

    init

---
+++ src/main/java/com/liuqh/netty/EchoClientHandler.java
@@ -0,0 +1,37 @@
+package com.liuqh.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.util.CharsetUtil;
+
+@Sharable
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+	/**
+	 * 此方法会在连接到服务器后被调用
+	 */
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) {
+		ctx.write(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
+	}
+
+	/**
+	 * 捕捉到异常
+	 */
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		cause.printStackTrace();
+		ctx.close();
+	}
+	/**
+	 * 接收到服务端的数据调用
+	 */
+	@Override
+	protected void messageReceived(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+		System.out.println("Client received: " + ByteBufUtil.hexDump(msg.readBytes(msg.readableBytes())));
+	}
+
+}
